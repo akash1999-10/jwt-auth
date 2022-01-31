b5e58f845381c62d476657d22926077fcc51a942
akash.solace <93201516+akash1999-10@users.noreply.github.com>
2022-01-31 18:00:01 +0530

    jwt auth

---
+++ client/src/App.js
@@ -0,0 +1,148 @@
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+function App() {
+  const [user, setUser] = useState({});
+  const [err, setErr] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const refresh = (refreshToken) => {
+    console.log("Refreshing token!");
+
+    return new Promise((resolve, reject) => {
+      axios
+        .post("http://localhost:5000/refresh", { token: refreshToken })
+        .then((data) => {
+          if (data.data.success === false) {
+            setErr("Login again");
+            // set message and return.
+            resolve(false);
+          } else {
+            const { accessToken } = data.data;
+            Cookies.set("access", accessToken);
+            resolve(accessToken);
+          }
+        });
+    });
+  };
+
+  const requestLogin = async (accessToken, refreshToken) => {
+    console.log(accessToken, refreshToken);
+    return new Promise((resolve, reject) => {
+      axios
+        .post(
+          "http://localhost:5000/protected",
+          {},
+          { headers: { authorization: `Bearer ${accessToken}` } }
+        )
+        .then(async (data) => {
+          if (data.data.success === false) {
+            if (data.data.message === "User not authenticated") {
+              setErr("Login again");
+              // set err message to login again.
+            } else if (data.data.message === "Access token expired") {
+              const accessToken = await refresh(refreshToken);
+              return await requestLogin(accessToken, refreshToken);
+            }
+
+            resolve(false);
+          } else {
+            // protected route has been accessed, response can be used.
+            setErr("Protected route accessed!");
+            resolve(true);
+          }
+        });
+    });
+  };
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+    console.log(user);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:5000/login", { user }).then((data) => {
+      const { accessToken, refreshToken } = data.data;
+      setIsSubmitted(true);
+      Cookies.set("access", accessToken);
+      Cookies.set("refresh", refreshToken);
+    });
+  };
+
+  const hasAccess = async (accessToken, refreshToken) => {
+    if (!refreshToken) return null;
+
+    if (accessToken === undefined) {
+      // generate new accessToken
+      accessToken = await refresh(refreshToken);
+      return accessToken;
+    }
+
+    return accessToken;
+  };
+
+  const protect = async (e) => {
+    let accessToken = Cookies.get("access");
+    let refreshToken = Cookies.get("refresh");
+
+    accessToken = await hasAccess(accessToken, refreshToken);
+
+    if (!accessToken) {
+      // Set message saying login again.
+    } else {
+      await requestLogin(accessToken, refreshToken);
+    }
+  };
+
+  const renderForm = (
+    <div className="form">
+      <form action="" onSubmit={handleSubmit} onChange={handleChange}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="email" name="email" placeholder="Email address"  required />
+          {/* {renderErrorMessage("uname")} */}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="password" placeholder="Password" required />
+          {/* {renderErrorMessage("pass")} */}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+
+  return (
+    <div className="app">
+      {/* <form action="" onChange={handleChange} onSubmit={handleSubmit}>
+                <input name="email" type="email" placeholder="Email address" />
+                <br />
+                <br />
+
+                <input name="password" type="password" placeholder="Password" />
+                <br />
+                <br />
+                <input type="submit" value="Login" />
+                <br />
+                <br />
+            </form> */}
+      <div className="login-form">
+        <div className="title">Sign In</div>
+        {isSubmitted ? <div>User Succesfully logged in</div> : renderForm}
+        {err}
+        <div className="access_btn">
+         <button onClick={protect} >Access Protected Content</button>
+         </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
